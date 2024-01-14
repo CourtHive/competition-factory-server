a38a5ae84921b889bf9ee2a9cae4489f5e38f254
courthive <charles@courthive.com>
2024-01-14 08:53:01 -0500

    feat: :sparkles: add role checking to socketGuard

--- src/auth/guards/socket.guard.ts
+++ src/auth/guards/socket.guard.ts
@@ -1,50 +1,43 @@
-//import { UsersService } from 'src/providers/users/users.service';
-import { CanActivate, Injectable } from '@nestjs/common';
+import { CanActivate, Injectable, ExecutionContext } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 
 import { IS_PUBLIC_KEY } from '../decorators/public.decorator';
+import { Roles } from '../decorators/roles.decorator';
 import { Reflector } from '@nestjs/core';
+import { Socket } from 'socket.io';
 
 @Injectable()
 export class SocketGuard implements CanActivate {
   constructor(
-    // private userService: UsersService,
     private jwtService: JwtService,
     private reflector: Reflector,
   ) {}
 
-  async canActivate(context: any): Promise<boolean> {
+  async canActivate(context: ExecutionContext): Promise<boolean> {
     const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
       context.getHandler(),
       context.getClass(),
     ]);
-    if (isPublic) {
-      return true;
+    if (isPublic) return true;
+
+    const socket: Socket = context.switchToWs().getClient();
+    const token = this.extractTokenFromContext(context);
+    if (!token) {
+      socket.emit('exception', { message: 'Not logged in or token expired' });
+      return false;
     }
 
-    const token = this.extractTokenFromContext(context);
-    if (!token) return false;
-    console.log({ token });
-
     try {
-      const payload = await this.jwtService.verifyAsync(token, {
+      const user = await this.jwtService.verifyAsync(token, {
         secret: process.env.JWT_SECRET,
       });
       return new Promise((resolve, reject) => {
-        console.log({ payload });
-        return payload ? resolve(true) : reject(false);
-        /*
-        return this.userService.findOne(decoded.username).then((user) => {
-          if (user) {
-            resolve(user);
-          } else {
-            reject(false);
-          }
-        });
-	*/
+        const roles = this.reflector.get(Roles, context.getHandler());
+        const hasRole = !roles || user.roles?.some((role) => !!roles.find((item) => item === role));
+        return hasRole ? resolve(true) : reject(false);
       });
     } catch (exception) {
-      console.log(exception);
+      socket.emit('exception', { message: exception });
       return false;
     }
   }
