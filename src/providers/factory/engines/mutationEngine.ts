d869001e195f98f83d13fe205e5835d00039b1f4
courthive <charles@courthive.com>
2024-01-17 16:02:42 -0500

    refactor: :recycle: update imports

--- src/providers/factory/engines/mutationEngine.ts
+++ src/providers/factory/engines/mutationEngine.ts
@@ -4,22 +4,7 @@ import asyncGlobalState from './asyncGlobalState';
 globalState.setStateProvider(asyncGlobalState);
 const engineAsync = asyncEngine(true);
 
-const methods = {
-  ...governors.competitionGovernor,
-  ...governors.participantGovernor,
-  ...governors.generationGovernor,
-  ...governors.publishingGovernor,
-  ...governors.tournamentGovernor,
-  ...governors.scheduleGovernor,
-  ...governors.policyGovernor,
-  ...governors.reportGovernor,
-  ...governors.mocksGovernor,
-  ...governors.eventGovernor,
-  ...governors.scoreGovernor,
-  ...governors.venueGovernor,
-};
-
-engineAsync.importMethods(methods);
+engineAsync.importMethods(governors, true, 1);
 
 export const mutationEngine = engineAsync;
 export default engineAsync;
