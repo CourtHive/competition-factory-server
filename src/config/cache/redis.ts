fe3b62ca22b20d41bac90bcc2738200eedcf6a26
courthive <charles@courthive.com>
2024-01-24 20:44:13 -0500

    feat: :sparkles: redis cache module

--- src/config/cache/redis.ts
+++ src/config/cache/redis.ts
@@ -1,5 +1,6 @@
 export default () => ({
   redis: {
+    url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
     username: process.env.REDIS_USERNAME,
     password: process.env.REDIS_PASSWORD,
     host: process.env.REDIS_HOST,
