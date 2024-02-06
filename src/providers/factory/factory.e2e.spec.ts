9e6bb4414888ddc41b6ae62bccb27167d305ff65
courthive <charles@courthive.com>
2024-02-06 09:55:05 -0500

    ci: :technologist: uppdate test config

--- src/providers/factory/factory.e2e.spec.ts
+++ src/providers/factory/factory.e2e.spec.ts
@@ -1,9 +1,9 @@
 import { INestApplication } from '@nestjs/common';
-import { AppModule } from '../../app.module';
+import { AppModule } from 'src/app.module';
 import { Test } from '@nestjs/testing';
 import request from 'supertest';
 
-import { TEST, TEST_EMAIL, TEST_PASSWORD } from '../../common/constants/test';
+import { TEST, TEST_EMAIL, TEST_PASSWORD } from 'src/common/constants/test';
 
 describe('FactoryService', () => {
   let app: INestApplication;
