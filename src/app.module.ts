4b02a22ed91425d5e72526986be5ea298ec10bd7
courthive <charles@courthive.com>
2024-02-07 20:57:19 -0500

    refactor: :recycle: cleanup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,15 @@
 import { ConversionModule } from './providers/conversion/conversion.module';
 import { FactoryModule } from './providers/factory/factory.module';
 import { MessagingModule } from './messaging/messaging.module';
+import { CacheModule } from './providers/cache/cache.module';
 import { UsersModule } from './providers/users/users.module';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { ConfigsModule } from './config/config.module';
 import { AuthModule } from './auth/auth.module';
-
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { Module } from '@nestjs/common';
-
 import { join } from 'path';
-import { CacheModule } from './providers/cache/cache.module';
 
 @Module({
   imports: [
