ebc9675b07d782b2efd8dac8d4d3e87b3d652da1
courthive <charles@courthive.com>
2024-02-09 09:21:09 -0500

    feat: :sparkles: update mutation engine

--- src/providers/factory/engines/mutationEngine.ts
+++ src/providers/factory/engines/mutationEngine.ts
@@ -1,10 +1,33 @@
-import { governors, asyncEngine, globalState } from 'tods-competition-factory';
+import { governors, asyncEngine, globalState, topicConstants } from 'tods-competition-factory';
 import asyncGlobalState from './asyncGlobalState';
 
+const traverse = true;
+const global = true;
+const depth = 1;
+
+globalState.setStateMethods(governors, traverse, depth, global); // globally imports methods from governors
 globalState.setStateProvider(asyncGlobalState);
-const engineAsync = asyncEngine(true);
+globalState.setGlobalSubscriptions({
+  subscriptions: {
+    // any subscriptions that don't need access to cacheManager can go here
+  },
+});
+asyncGlobalState.createInstanceState(); // is there only one instance of asyncGlobalState?
 
-engineAsync.importMethods(governors, true, 1);
+export function getMutationEngine(cacheManager?) {
+  const engineAsync = asyncEngine();
+  globalState.setSubscriptions({
+    subscriptions: {
+      [topicConstants.PUBLISH_EVENT]: (params) => {
+        if (Array.isArray(params)) {
+          for (const item of params) {
+            const key = `ged|${item.tournamentId}|${item.eventData.eventInfo.eventId}`;
+            cacheManager?.set(key, item, 60 * 3 * 1000); // 3 minutes
+          }
+        }
+      },
+    },
+  });
 
-export const mutationEngine = engineAsync;
-export default engineAsync;
+  return engineAsync;
+}
