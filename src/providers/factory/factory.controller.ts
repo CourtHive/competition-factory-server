d3589f4a92e13a090a592c10ed48cb93a72db63f
courthive <charles@courthive.com>
2024-02-09 08:03:47 -0500

    fix: :bug: iclude CACHE_MANAGER in test setup

--- src/providers/factory/factory.controller.ts
+++ src/providers/factory/factory.controller.ts
@@ -4,18 +4,33 @@ import { QueryTournamentRecordsDto } from './dto/queryTournamentRecords.dto';
 import { SaveTournamentRecordsDto } from './dto/saveTournamentRecords.dto';
 import { GetTournamentInfoDto } from './dto/getTournamentInfo.dto';
 import { ExecutionQueueDto } from './dto/executionQueue.dto';
+import { GetEventDataDto } from './dto/getEventData.dto';
 
-import { Controller, Get, Post, HttpCode, HttpStatus, Body, UseGuards, Param } from '@nestjs/common';
+import { Controller, Get, Post, HttpCode, HttpStatus, Body, UseGuards, Inject, Param } from '@nestjs/common';
 import { Public } from 'src/auth/decorators/public.decorator';
+import { CACHE_MANAGER, Cache } from '@nestjs/cache-manager';
 import { Roles } from 'src/auth/decorators/roles.decorator';
 import { RolesGuard } from 'src/auth/guards/role.guard';
 import { FactoryService } from './factory.service';
-import { GetEventDataDto } from './dto/getEventData.dto';
 
 @UseGuards(RolesGuard)
 @Controller('factory')
 export class FactoryController {
-  constructor(private readonly factoryService: FactoryService) {}
+  constructor(
+    private readonly factoryService: FactoryService,
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
+  ) {}
+
+  async cachFx(key, fx, params) {
+    const cachedData: any = await this.cacheManager.get(key);
+    if (cachedData) {
+      if (typeof cachedData === 'object') cachedData._cached = true;
+      return cachedData;
+    }
+    const result = await fx(params);
+    if (!result.error) this.cacheManager.set(key, result, 60 * 3 * 1000); // 3 minutes
+    return result;
+  }
 
   @Get()
   @Public()
