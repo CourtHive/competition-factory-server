9c70cca4c5733793211ce10b5abece5db132fc74
courthive <charles@courthive.com>
2024-02-12 17:14:50 -0500

    fix: :bug: correct condition for match upload

--- src/messaging/tracker/tracker.gateway.ts
+++ src/messaging/tracker/tracker.gateway.ts
@@ -55,7 +55,7 @@ export class TrackerGateway {
   async messageHandler(@MessageBody() data: any, @ConnectedSocket() client: Socket): Promise<any> {
     if (typeof data !== 'object') return { notFound: data };
     const { type, payload = {} } = data;
-    if (messages['executionQueue']) {
+    if (messages[type]) {
       return messages[type]({ client, payload });
     } else {
       this.logger.debug(`Not found: ${type}`);
