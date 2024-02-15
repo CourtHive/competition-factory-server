e05c38c0e2e46ce05075984365e6876f6115f5df
courthive <charles@courthive.com>
2024-02-15 14:26:33 -0500

    test: :white_check_mark: update tests

---
+++ src/modules/app/app.e2e.spec.ts
@@ -0,0 +1,26 @@
+import { AppModule } from 'src/modules/app/app.module';
+import { INestApplication } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+import request from 'supertest';
+
+describe('Static Files', () => {
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleRef.createNestApplication();
+    await app.init();
+  });
+
+  it('/GET /', async () => {
+    const result = await request(app.getHttpServer()).get('/').expect(200);
+    expect(result.body).toEqual({ message: 'Factory server' });
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+});
