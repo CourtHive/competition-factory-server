deb48835f207ca3cf60d14ef9b5ccdf1f0d27980
courthive <charles@courthive.com>
2024-02-16 09:52:31 -0500

    test: :white_check_mark: correct `getTournamentMatchUps` and extend test coverage

--- src/modules/factory/functions/private/queryTournamentRecords.spec.ts
+++ src/modules/factory/functions/private/queryTournamentRecords.spec.ts
@@ -1,10 +1,12 @@
 import { generateTournamentRecord } from '../../../../services/fileSystem/generateTournamentRecord';
 import { removeTournamentRecords } from '../../../../services/fileSystem/removeTournamentRecords';
 import { queryTournamentRecords } from './queryTournamentRecords';
+import { getTournamentInfo } from '../public/getTournamentInfo';
 import { TEST } from '../../../../common/constants/test';
 import fileStorage from 'src/services/fileSystem';
 import levelStorage from 'src/services/levelDB';
 import 'dotenv/config';
+import { getTournamentMatchUps } from '../public/getTournamentMatchUps';
 
 describe('queryTournamentRecords', () => {
   const storage = process.env.APP_STORAGE === 'levelDB' ? levelStorage : fileStorage;
@@ -15,7 +17,7 @@ describe('queryTournamentRecords', () => {
     expect(result.success).toEqual(true);
 
     // SECOND: generate a tournamentRecord with this tournamentId and persist to storage
-    result = generateTournamentRecord({
+    result = await generateTournamentRecord({
       tournamentAttributes: { tournamentId: TEST },
       drawProfiles: [{ drawSize: 16 }],
     });
@@ -30,6 +32,18 @@ describe('queryTournamentRecords', () => {
       },
       { storage },
     );
+    expect(result.tournamentInfo).toBeDefined();
+    expect(result.success).toEqual(true);
+
+    result = await getTournamentInfo({ tournamentId: TEST }, { storage });
+    expect(result.tournamentInfo).toBeDefined();
+    expect(result.success).toEqual(true);
+
+    result = await getTournamentMatchUps({ tournamentId: TEST }, { storage });
+    expect(result.success).toEqual(true);
+
+    // FOURTH: remove the tournamentRecord
+    result = await removeTournamentRecords({ tournamentId: TEST });
     expect(result.success).toEqual(true);
   });
 });
--- src/modules/factory/functions/public/getTournamentInfo.ts
+++ src/modules/factory/functions/public/getTournamentInfo.ts
@@ -1,10 +1,15 @@
 import { queryGovernor } from 'tods-competition-factory';
 
+import { SUCCESS } from 'src/common/constants/app';
+
 export async function getTournamentInfo({ tournamentId }, services?: any) {
   if (!tournamentId) return { error: 'MISSING_TOURNAMENT_ID' };
   const findResult = await services.storage.findTournamentRecord({ tournamentId });
   if (findResult.error) return findResult;
-  const infoResult = queryGovernor.getTournamentInfo({ tournamentRecord: findResult.tournamentRecord });
+  const infoResult = queryGovernor.getTournamentInfo({
+    tournamentRecord: findResult.tournamentRecord,
+    usePublishState: true,
+  });
   if (infoResult.error) return infoResult;
-  return infoResult.tournamentInfo;
+  return { ...SUCCESS, tournamentInfo: infoResult.tournamentInfo };
 }
