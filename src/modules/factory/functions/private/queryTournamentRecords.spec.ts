0d941241d8c323bc9dbbcbca19d56bac6c875a12
courthive <charles@courthive.com>
2024-02-16 17:55:09 -0500

    refactor: :fire: remove direct use of fileSystem

--- src/modules/factory/functions/private/queryTournamentRecords.spec.ts
+++ src/modules/factory/functions/private/queryTournamentRecords.spec.ts
@@ -1,16 +1,12 @@
 import { generateTournamentRecord } from '../../../../services/fileSystem/generateTournamentRecord';
 import { removeTournamentRecords } from '../../../../services/fileSystem/removeTournamentRecords';
+import { getTournamentMatchUps } from '../public/getTournamentMatchUps';
 import { queryTournamentRecords } from './queryTournamentRecords';
 import { getTournamentInfo } from '../public/getTournamentInfo';
 import { TEST } from '../../../../common/constants/test';
-import fileStorage from 'src/services/fileSystem';
-import levelStorage from 'src/services/levelDB';
 import 'dotenv/config';
-import { getTournamentMatchUps } from '../public/getTournamentMatchUps';
 
 describe('queryTournamentRecords', () => {
-  const storage = process.env.APP_STORAGE === 'levelDB' ? levelStorage : fileStorage;
-
   it('can query a tournamentRecord', async () => {
     // FIRST: remove any existing tournamentRecord with this tournamentId
     let result: any = await removeTournamentRecords({ tournamentId: TEST });
@@ -24,22 +20,19 @@ describe('queryTournamentRecords', () => {
     expect(result.success).toEqual(true);
 
     // THIRD: execute a directive on the tournamentRecord
-    result = await queryTournamentRecords(
-      {
-        params: { tournamentId: TEST },
-        method: 'getTournamentInfo',
-        tournamentId: TEST,
-      },
-      { storage },
-    );
+    result = await queryTournamentRecords({
+      params: { tournamentId: TEST },
+      method: 'getTournamentInfo',
+      tournamentId: TEST,
+    });
     expect(result.tournamentInfo).toBeDefined();
     expect(result.success).toEqual(true);
 
-    result = await getTournamentInfo({ tournamentId: TEST }, { storage });
+    result = await getTournamentInfo({ tournamentId: TEST });
     expect(result.tournamentInfo).toBeDefined();
     expect(result.success).toEqual(true);
 
-    result = await getTournamentMatchUps({ tournamentId: TEST }, { storage });
+    result = await getTournamentMatchUps({ tournamentId: TEST });
     expect(result.success).toEqual(true);
 
     // FOURTH: remove the tournamentRecord
