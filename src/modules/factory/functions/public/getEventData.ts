c0000a83c5fe7378f225f4ca66907baba5ecedf5
CourtHive <courthive@gmail.com>
2024-03-06 11:58:47 -0500

    refactor: :lock: add privacy policy to `getEventData`

--- src/modules/factory/functions/public/getEventData.ts
+++ src/modules/factory/functions/public/getEventData.ts
@@ -1,15 +1,24 @@
+import { queryGovernor, fixtures } from 'tods-competition-factory';
 import levelStorage from 'src/services/levelDB';
-import { queryGovernor } from 'tods-competition-factory';
 
 export async function getEventData(params: any) {
   if (!params.tournamentId) return { error: 'MISSING_TOURNAMENT_ID' };
   const findResult = await levelStorage.findTournamentRecord({ tournamentId: params.tournamentId });
   if (findResult.error) return findResult;
+  const policyDefinitions = fixtures.policies.POLICY_PRIVACY_DEFAULT;
+  policyDefinitions.participant.participant.person.sex = true;
+  policyDefinitions.participant.participant.rankings = true;
+  policyDefinitions.participant.participant.ratings = true;
   const infoResult = queryGovernor.getEventData({
+    participantsProfile: { withScaleValues: true },
     tournamentRecord: findResult.tournamentRecord,
+    allParticipantResults: true,
     eventId: params.eventId,
     usePublishState: true,
+    pressureRating: true,
+    refreshResults: true,
+    policyDefinitions,
   });
   if (infoResult.error) return infoResult;
-  return infoResult.eventData;
+  return infoResult;
 }
