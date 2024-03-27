a9b306ba5ded215060a9c4e6fc581ebabfde03ca
CourtHive <courthive@gmail.com>
2024-03-27 12:38:19 -0400

    fix: :bug: corrections for `getParticipants` route

--- src/modules/factory/functions/public/getParticipants.ts
+++ src/modules/factory/functions/public/getParticipants.ts
@@ -5,25 +5,25 @@ import levelStorage from 'src/services/levelDB';
 import { SUCCESS } from 'dist/common/constants/app';
 
 export async function getParticipants(params) {
-  const { tournamentId, ...opts } = params;
+  const { tournamentId, ...opts } = params ?? {};
   if (!tournamentId) return { error: 'MISSING_TOURNAMENT_ID' };
 
-  const findResult: any = await levelStorage.fetchTournamentRecords({ tournamentId });
+  const findResult: any = await levelStorage.findTournamentRecord({ tournamentId });
   if (findResult.error) return findResult;
 
-  const tournamentRecords = findResult.tournamentRecords;
+  const tournamentRecord = findResult.tournamentRecord;
 
-  const pubStatus = queryGovernor.getTournamentPublishStatus({ tournamentRecords });
+  const pubStatus = queryGovernor.getTournamentPublishStatus({ tournamentRecord });
   if (!pubStatus?.participants?.published) return { error: 'Participants not published' };
 
   const participantResult = queryGovernor.getParticipants({
-    policyDefinitions: fixtures.policies.POLICY_PRIVACTY_DEFAULT,
+    policyDefinitions: fixtures.policies.POLICY_PRIVACY_DEFAULT,
     contextFilters: opts?.contextFilters,
     matchUpFilters: opts?.matchUpFilters,
     activeTournamentId: tournamentId,
     withScaleValues: true,
     usePublishState: true, // filters out events that are not published
-    tournamentRecords,
+    tournamentRecord,
     withEvents: true,
     // withISO2: true,
     // withIOC: true,
