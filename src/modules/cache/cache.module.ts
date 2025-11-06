e693a96faaf3000521e692424561912a30eaf115
CourtHive <courthive@gmail.com>
2025-11-06 11:37:52 -0500

    build: :arrow_up: upgrade dependencies and refactor for major version bumps

--- src/modules/cache/cache.module.ts
+++ src/modules/cache/cache.module.ts
@@ -1,16 +1,16 @@
 import { CacheModule as CacheModule_, CacheModuleOptions } from '@nestjs/cache-manager';
-import { redisStore } from 'cache-manager-redis-yet';
-import type { RedisClientOptions } from 'redis';
+import Keyv from 'keyv';
+import KeyvRedis from '@keyv/redis';
 import { Module, Global } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 
-export const configCacheFactory = (config: ConfigService): CacheModuleOptions<RedisClientOptions> => {
+export const configCacheFactory = (config: ConfigService): CacheModuleOptions => {
   const redisConfig = config.get('redis');
   const url = redisConfig?.url || 'redis://127.0.0.1:6379';
-  const ttl = redisConfig?.ttl || 60 * 60 * 24 * 7;
+  const ttl = redisConfig?.ttl || 60 * 60 * 24 * 7 * 1000; // milliseconds
 
   return {
-    store: () => redisStore({ ttl, url }),
+    stores: [new Keyv({ store: new KeyvRedis(url), ttl })],
     isGlobal: true,
     max: 10_000,
   };
--- src/modules/factory/helpers/checkProvider.ts
+++ src/modules/factory/helpers/checkProvider.ts
@@ -4,7 +4,7 @@ export function checkProvider({ tournamentRecords, user }) {
   if (user?.roles?.includes(SUPER_ADMIN)) return true;
   for (const tournamentId in tournamentRecords ?? {}) {
     const providerId = tournamentRecords[tournamentId]?.parentOrganisation?.organisationId;
-    const providerIds = user?.providerIds ?? user.providerId ? [user.providerId] : [];
+    const providerIds = (user?.providerIds ?? user.providerId) ? [user.providerId] : [];
     if (providerId && !providerIds?.includes(providerId)) return false;
   }
   return true;
